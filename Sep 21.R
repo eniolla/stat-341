cc0aad62e004c3d3c31e0e870ca547007d30d45b
eniolla <112187117+eniolla@users.noreply.github.com>
2022-09-24 15:51:48 -0400

    Add files via upload

---
+++ Sep 21.R
@@ -0,0 +1,10 @@
+#Author: Fatokun, Date: 09/21/2022, purpose: Perform ANOVA analysis on sample dataset
+
+PATH <- "https://raw.githubusercontent.com/guru99-edu/R-Programming/master/poisons.csv" 
+
+df <- read.csv(PATH)
+
+df
+
+anova_one_way <- aov(time~poison, data = df);
+summary(anova_one_way)
